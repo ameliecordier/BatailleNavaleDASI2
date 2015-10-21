17b1a04931bdca810cba258e7485275b9d60f228
acordier <acordier@gmail.com>
2015-10-21 12:05:29 +0200

    Refactoring et premier test unitaire...

--- bataille.py
+++ bataille.py
@@ -1,30 +1,59 @@
-print("Bienvenue dans ce jeu de Bataille Navale d'enfer !")
-reponse = input("Voulez-vous jouer avec moi ? (O / N) ")
+import unittest
 
-if reponse == "O":
-    print("Youpi, let's go !")
-    X = input("Quel est la taille de X")
-    Y = input("Quel est la taille de Y")
-    print("La valeur de X est : " + X + " et la valeur de Y est " + Y)
 
-    ligne = []
-    ocean = []
+#Propre
+def afficherOcean(ocean):
+    for i in range(0,len(ocean)):
+       print(" ".join(ocean[i]))
 
-    for i in range(0,int(X)):
+def construireLigne(x):
+    for i in range(0, x):
         ligne.append("o")
+    return ligne    
 
-    for i in range(0,int(Y)):
-        ocean.append(ligne)
+def construireOcean(x, y):
+    l = construireLigne(x)
+    for i in range(0, y):        
+        ocean.append(l)
+    return ocean
 
-    for i in range(0,int(Y)):
-        print(" ".join(ocean[i]))
-else:
-    print("Oh noooon... personne ne veut jamais jouer avec moi !")
+def deciderDeJouer():
+    print("Bienvenue dans ce jeu de Bataille Navale d'enfer !")
+    reponse = input("Voulez-vous jouer avec moi ? (O / N) ")
+    
+    if reponse == "O":
+        print("Youpi")
+    elif reponse == "N":
+        print("Tant pis")
+    else:
+        print("Je vous repose la question ?")
+        reponse = deciderDeJouer()
+    return reponse
+
+def demanderTailleOcean():
+    X = input("Quel est la taille de X ? ")
+    Y = input("Quel est la taille de Y ? ")
+    print("Pour info, vous avez demandé un océan de " + X + "x" + Y)
+    return int(X), int(Y)
+
+#Programme principal
+ligne = [] # A refactorer la prochaine fois... parce que c'est pas top
+ocean = []
+jouer = deciderDeJouer()
+if jouer == "O":
+    tailleX, tailleY = demanderTailleOcean()
+    ocean=construireOcean(tailleX, tailleY)    
+    afficherOcean(ocean)
 
 
-l = []
-l.append("toto")
-l.append("tata")
-print(",".join(l))
 
-print(" ".join(l))
+class TestBatailleMethods(unittest.TestCase):
+
+    def test_constructionOcean(self):
+        monOcean = construireOcean(3,7)
+        self.assertEqual(len(monOcean),7)
+        self.assertEqual(len(monOcean[0]),3)
+
+  
+unittest.main()
+
