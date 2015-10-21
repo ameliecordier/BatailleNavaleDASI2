e46643b7f23f1068471b681e2ae917f0fd77a318
acordier <acordier@gmail.com>
2015-10-21 11:05:42 +0200

    Damien

--- bataille.py
+++ bataille.py
@@ -6,5 +6,25 @@ if reponse == "O":
     X = input("Quel est la taille de X")
     Y = input("Quel est la taille de Y")
     print("La valeur de X est : " + X + " et la valeur de Y est " + Y)
+
+    ligne = []
+    ocean = []
+
+    for i in range(0,int(X)):
+        ligne.append("o")
+
+    for i in range(0,int(Y)):
+        ocean.append(ligne)
+
+    for i in range(0,int(Y)):
+        print(" ".join(ocean[i]))
 else:
     print("Oh noooon... personne ne veut jamais jouer avec moi !")
+
+
+l = []
+l.append("toto")
+l.append("tata")
+print(",".join(l))
+
+print(" ".join(l))
