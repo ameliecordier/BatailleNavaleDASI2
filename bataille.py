c9407eabc6c1e5873a72515d67c1c883f030d3ec
acordier <acordier@gmail.com>
2015-10-21 10:44:28 +0200

    Nicolas David

--- bataille.py
+++ bataille.py
@@ -3,5 +3,8 @@ reponse = input("Voulez-vous jouer avec moi ? (O / N) ")
 
 if reponse == "O":
     print("Youpi, let's go !")
+    X = input("Quel est la taille de X")
+    Y = input("Quel est la taille de Y")
+    print("La valeur de X est : " + X + " et la valeur de Y est " + Y)
 else:
     print("Oh noooon... personne ne veut jamais jouer avec moi !")
