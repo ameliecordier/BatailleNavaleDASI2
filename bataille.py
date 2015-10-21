e625c1ff05fd37fbff2b9c9f22d866a8cbda95f9
acordier <acordier@gmail.com>
2015-10-21 10:32:32 +0200

    Intro de la bataille navale

---
+++ bataille.py
@@ -0,0 +1,7 @@
+print("Bienvenue dans ce jeu de Bataille Navale d'enfer !")
+reponse = input("Voulez-vous jouer avec moi ? (O / N) ")
+
+if reponse == "O":
+    print("Youpi, let's go !")
+else:
+    print("Oh noooon... personne ne veut jamais jouer avec moi !")
