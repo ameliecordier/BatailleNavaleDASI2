59ec8f91994954964d5e47ec6d407d0ef371139d
Anihithe <anihithe@msn.com>
2015-11-26 14:36:28 +0100

    Prototype TPT

---
+++ joueurtpt.py
@@ -0,0 +1,15 @@
+class Joueur:
+
+    def __init__(self, nom, HP):
+        self.nom = nom
+        self.HP = HP
+
+    def tour(joueur):
+        if(joueur.hp > 0):
+            #appel fonction tir
+
+#Main
+
+    while(J1.HP > 0 and J2.HP > 0):
+        joueurtpt.tour(J1)
+        joueurtpt.tour(J2)
