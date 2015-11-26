f1780d9995365ffdb387da349933194c0d1cb017
quentin <quentinmeltzheim@gmail.com>
2015-11-26 15:56:34 +0100

    Bonne base

---
+++ Arsenal.py
@@ -0,0 +1,18 @@
+class Arsenal():
+
+    def __init__(self):
+        self.bateaux = []
+        self.creerDictionnaire()
+        self.arsenal = None
+
+
+    def creerDictionnaire(self):
+        self.dict = dict()
+        self.dict["Torpilleur"] = 2
+        self.dict["Sous-marin"] = 3
+        self.dict["Contre-torpilleur"] = 3
+        self.dict["Croiseur"] = 4
+        self.dict["Porte-avion"] = 5
+
+    def ajouterBateau(self,bateau):
+        self.bateaux.append(bateau)
