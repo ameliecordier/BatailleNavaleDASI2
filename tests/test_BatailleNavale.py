4ed7ab7bbde98ef8e41edd84858367b7fec2bcf7
Nicolas David <nico01.david@gmail.com>
2015-12-07 09:37:08 +0100

    Réparation test_BatailleNavale.py

--- tests/test_BatailleNavale.py
+++ tests/test_BatailleNavale.py
@@ -8,13 +8,4 @@ class TestBatailleNavale(unittest.TestCase):
         monBeauBateau.configure("porte-avion")
         self.assertEqual(monBeauBateau.size, 5)
 
-<<<<<<< HEAD
-    def test_monTest(self):
-        self.assertEqual(2+2,5)
-
-    def test_Erreur(self):
-        print toto
-=======
->>>>>>> origin/master
-
 #unittest.main()
