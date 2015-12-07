d611b6c6ed96fa9f65ec4fc3abf3cd7f02537573
Robin <Robin@Robin-Laptop>
2015-12-07 09:07:16 +0100

    Revert "A"
    This reverts commit 7d50d92fbf01beed184e76bcbfea07a0b0b49d53.

--- tests/test_Bateau.py
+++ tests/test_Bateau.py
@@ -5,11 +5,11 @@ import Arsenal
 class TestBateau(unittest.TestCase):
     def test_constructionBateau(self):
         arsenal = Arsenal.Arsenal()
-        bateau = flotte.Bateau(arsenal, "porte-avion")
+        bateau = flotte.Bateau(Arsenal, "porte-avion")
         self.assertEqual(bateau.taille,5)
     def test_ajoutBateau(self):
         arsenal = Arsenal.Arsenal()
-        bateau = flotte.Bateau(arsenal, "torpilleur")
+        bateau = flotte.Bateau(Arsenal, "torpilleur")
         self.assertEqual(bateau, arsenal.bateaux[0])
 
 
