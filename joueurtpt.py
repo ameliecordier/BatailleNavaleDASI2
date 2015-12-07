d3e9590f1efbc23240727fb3c95345b42f834502
Anihithe <anihithe@msn.com>
2015-12-07 08:47:46 +0100

    Suppr __pycache__

---
+++ joueurtpt.py
@@ -0,0 +1,21 @@
+class Joueur:
+
+    def __init__(self, nom, HP):
+        self.nom = nom
+        self.HP = HP
+
+    def isAlive():
+        if(self.HP > 0):
+            return True
+        else:
+            return False
+        
+        
+#Main
+
+    while(J1.isAlive and J2.isAlive):
+        tire(J1, XY)
+        if (J2.isAlive):
+            tire(J2, XY)
+    #Appel fonction win
+        
